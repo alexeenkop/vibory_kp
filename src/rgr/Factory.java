b87e5a82fea340f02ac86952707b4eb327bbb53a
Petia <alexeenkop77@gmail.com>
2015-05-25 01:51:32 +0300

    ver 1

---
+++ src/rgr/Factory.java
@@ -0,0 +1,17 @@
+package rgr;
+
+import process.Dispatcher;
+//import qusystem.IModelFactory;
+
+public class Factory implements process.IModelFactory {
+	private Main gui;
+
+	public Factory(Main gui) {
+		this.gui = gui;
+	}
+	public Model createModel(Dispatcher dispatcher) {
+		Model newModel = new Model(dispatcher, gui);
+		return newModel;
+	}
+}
+
